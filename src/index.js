d1437639c5d9e0ad0b3a1c5230d45721d494c12c
NileshPatil-18 <patilnilesharjun@gmail.com>
2025-01-03 20:49:22 +0530

    final code

--- src/index.js
+++ src/index.js
@@ -13,13 +13,9 @@ app.use(express.urlencoded({ extended: false }));
 
 // Connect to DATABASE
 const DATABASE_URL = process.env.MONGO_DB_URL
-mongoose.connect(DATABASE_URL, { useNewUrlParser: true});
+mongoose.connect(DATABASE_URL);
 const db = mongoose.connection
 db.on('error', (err) => console.log(err))
 db.once('open', () => console.log('connected to database'))
-//
-
-
-
 // Start Server
 app.listen(port, () => console.log(`App listening on port ${port}!`))
